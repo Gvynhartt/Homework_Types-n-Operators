90640f1dee1b6c702108407d4566cc28c0b7817c
Gvynhartt <savshush134@gmail.com>
2023-01-18 08:30:42 +0300

    topup bonus program written

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        double TicketPrice = 15928.99;
-        int RoublesPerMile = 20;
-        int MileageBonus = (int)(TicketPrice / RoublesPerMile);
-        System.out.println(MileageBonus);
+        double InitialBalance = 300; // изначальная сумма на счету
+        double TopUpAmount = 928; // сумма пополнения счёта
+        boolean IsBonusApplicable = false; // логическая переменная, показывающая, доступен ли бонус
+
+        if (TopUpAmount > 1000) {
+            IsBonusApplicable = true;
+        } else {
+            IsBonusApplicable = false;
+        }
+
+        int OperatorBonus = IsBonusApplicable ? (int) (TopUpAmount / 100) : 0; // подсчитывается итоговый бонус от оператора
+        System.out.println("Итоговый баланс на счету: " + (InitialBalance + TopUpAmount + OperatorBonus) + " рублей; бонус от оператора: " + OperatorBonus + " рублей.");
     }
 }
