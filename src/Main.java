c5f0cb2e50b2d8f8ca2b83e26ec1942fa085151d
Gvynhartt <122565416+Gvynhartt@users.noreply.github.com>
2023-01-19 00:51:30 +0300

    Variable names now begin with lower case

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        double TicketPrice = 15928.99; // стоимость билета - дробная, потому что реализм
-        int RoublesPerMile = 20; // сколько рублей нужно на каждую бонусную милю
-        int MileageBonus = (int)(TicketPrice / RoublesPerMile); // подсчитывает итоговое число бонусных миль
-        System.out.println(MileageBonus);
+        double ticketPrice = 15928.99; // стоимость билета - дробная, потому что реализм
+        int roublesPerMile = 20; // сколько рублей нужно на каждую бонусную милю
+        int mileageBonus = (int)(ticketPrice / roublesPerMile); // подсчитывает итоговое число бонусных миль
+        System.out.println(mileageBonus);
     }
 }
