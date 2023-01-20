11153b69e449e44d9ef5ef8dc8e421a7b8e5fbf2
Gvynhartt <savshush134@gmail.com>
2023-01-20 05:40:16 +0300

    mileage project re-written with an object

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        double ticketPrice = 15928.99; // стоимость билета - дробная, потому что реализм
-        int roublesPerMile = 20; // сколько рублей нужно на каждую бонусную милю
-        int mileageBonus = (int)(ticketPrice / roublesPerMile); // подсчитывает итоговое число бонусных миль
-        System.out.println(mileageBonus);
+        BonusMilesService mileageBonus = new BonusMilesService();
+        double cost = 15928.99; // стоимость билета - дробная, потому что реализм
+        int price = mileageBonus.calculate((int)cost); // подсчитывает итоговое число бонусных миль
+        System.out.println("Ваше число бонусных миль: " + price);
     }
 }
